b665bced62081831cb32403b5b23672eec6e85aa
Kali Vanderlaan <s242667@dtu.dk>
2025-02-25 13:52:56 +0100

    Adding load_turbie_parameters function

--- codecamp/__init__.py
+++ codecamp/__init__.py
@@ -19,7 +19,18 @@ def load_wind(path_wind,t_start=0):
     t_wind,u_wind = np.hsplit(data,2)
     return t_wind, u_wind
 
-# DATA_DIR = Path('./data')
-# path_resp_file = DATA_DIR / 'resp_12_ms_TI_0.1.txt'
-# data1= load_resp(path_resp_file)
-# print(data1)
+def load_turbie_parameters(path_turbie): 
+    turbie_dict = {}
+    with open(path_turbie) as f:
+        next(f) 
+        for line in f: 
+            print(line)
+            value, *key = line.split()
+            turbie_dict[key] = [value]
+    return turbie_dict
+
+
+DATA_DIR = Path('./data')
+path_resp_file = DATA_DIR / 'turbie_parameters.txt'
+data1= load_turbie_parameters(path_resp_file)
+print(data1)
