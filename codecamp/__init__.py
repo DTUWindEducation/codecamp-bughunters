c43e0377616f20491a291631a4c621b682f1b9d4
Kali Vanderlaan <s242667@dtu.dk>
2025-02-26 11:45:38 +0100

    Deleting load_turbie_parameters before pushing to main

--- codecamp/__init__.py
+++ codecamp/__init__.py
@@ -19,26 +19,3 @@ def load_wind(path_wind,t_start=0):
     data = data[data[:,0]>=t_start] 
     t_wind,u_wind = np.hsplit(data,2)
     return t_wind, u_wind
-
-def load_turbie_parameters(path_turbie): 
-    file_content = '' 
-    with open(path_turbie, 'r') as f:
-        lines = f.readlines()
-        file_content = ''.join(lines)
-    for line in lines:
-        if 
-        print(line)
-    # turbie_dict = {}
-    # with open(path_turbie) as f:
-    #     next(f) 
-    #     for line in f: 
-    #         print(line)
-    #         value, *key = line.split()
-    #         turbie_dict[key] = [value]
-    return file_content # return turbie_dict
-
-
-DATA_DIR = Path('./data')
-path_resp_file = DATA_DIR / 'turbie_parameters.txt'
-data1= load_turbie_parameters(path_resp_file)
-print(data1)
