fb7e5b9c2d793925bd8462e3074bf70447ce37a4
Kali Vanderlaan <s242667@dtu.dk>
2025-03-10 22:35:49 +0100

    Adding work for part 1 and part 2 code for project

--- main.py
+++ main.py
@@ -10,23 +10,65 @@
     # Extra credit: make your main.py such that it can process/visualize results from all 3 TIs.
 
 
+# importing necessary libraries 
+from pathlib import Path
+import numpy as np
+import os 
+from datetime import datetime
+
+# importing functions 
+import codecamp
+
+# recording time at start of code 
+start_time = datetime.now()
+
 # pseudocode below: 
 
 # step 1: 
-# use load_turbie_parameters to load params 
+# use load_turbie_parameters to load params
+DATA_DIR = Path('./data')
+TI_1_DIR = Path('./data/wind_TI_0.1')
+turbie_params = DATA_DIR/ 'turbie_parameters.txt' 
+path_ct = DATA_DIR/'CT.txt'
+
 # initalize two empty arrays (blade_mean_stdv and tower_mean_stdv) to be used to store standard deviations and means for each wind speed data set 
+blade_data = []
+tower_data = []
 
 # step 2: 
 # create a loop which, for each wind speed file for TI = 0.1, does the following: 
-#   use load_wind to load the wind data 
-#   calculate Ct from CT curve and wind time series data 
+for file in os.listdir(TI_1_DIR): 
+    path_wind = TI_1_DIR/(str(file))
 
-#   call get_turbie_system_matricies 
-#   call simulate_turbie to get time, wind, and deflections 
-#   calculate the mean and standard deviation of the blade
-#   calculate the mean and stadard deviation of the tower 
-#   append tower_mean_stdv and blade_mean_stdv with mean and stdv values
+    #   use load_wind to load the wind data 
+    t_wind, u_wind = codecamp.load_wind(path_wind)
+
+    #  calculate Ct from CT curve and wind time series data 
+    ct = codecamp.calculate_ct(u_wind,path_ct)
+
+    #  call get_turbie_system_matricies 
+    M, C, K = codecamp.get_turbie_system_matrices(turbie_params)
+
+    #  call simulate_turbie to get time, wind, and deflections 
+    t, wind, xb, xt = codecamp.simulate_turbie(path_wind, turbie_params,path_ct)
+
+    #  calculate the mean and standard deviation of the blade
+    mean_blade = np.mean(xb)
+    std_blade = np.std(xb)
+
+    #  calculate the mean and stadard deviation of the tower
+    mean_tower = np.mean(xt)
+    std_tower = np.std(xt)
+
+    #  append tower_mean_stdv and blade_mean_stdv with mean and stdv values
+    blade_data.append([mean_blade,std_blade])
+    tower_data.append([mean_tower, std_tower])
 # end loop 
+
+blade_mean_stdv = np.array(blade_data)
+tower_mean_stdv = np.array(tower_data)
+print(blade_mean_stdv)
+print(tower_mean_stdv)
 
 # step 3:
 # create two figures (see reference link for method for plotting): 
@@ -34,5 +76,11 @@
 #   one which plots the means's and stdv's for the tower 
 #       reference link (https://stackoverflow.com/questions/22481854/plot-mean-and-standard-deviation)
 #   the figures should have the wind speed on the x axis (ex: 1, 2, 3, 4,.... [m/s])
+
+# recording time at end of code
+end_time = datetime.now()
+
+# printing execution time to verify running in under 10 mins 
+print('Run time: {}'.format(end_time - start_time))
 
 
