ad4e66e6f7b0ee2a259893a765c404d0e8851433
Kali Vanderlaan <s242667@dtu.dk>
2025-03-11 11:09:31 +0100

    adding code for part 1 and part 2 that sucessfully calculates the mean and stdv for each wind speed

--- main.py
+++ main.py
@@ -28,17 +28,28 @@ start_time = datetime.now()
 # use load_turbie_parameters to load params
 DATA_DIR = Path('./data')
 TI_1_DIR = Path('./data/wind_TI_0.1')
+wind_directory = os.listdir(TI_1_DIR)
 turbie_params = DATA_DIR/ 'turbie_parameters.txt' 
 path_ct = DATA_DIR/'CT.txt'
 
 # initalize two empty arrays (blade_mean_stdv and tower_mean_stdv) to be used to store standard deviations and means for each wind speed data set 
-blade_data = []
-tower_data = []
+blade_data_TI_01 = []
+tower_data_TI_01 = []
+
+# Get all .txt files in the folder
+files = [f for f in os.listdir(TI_1_DIR) if f.endswith('.txt')]
+
+# Sort the files by extracting the wind speed value (which is the second element when splitting by '_')
+files.sort(key=lambda x: int(x.split('_')[1]))
 
 # step 2: 
 # create a loop which, for each wind speed file for TI = 0.1, does the following: 
-for file in os.listdir(TI_1_DIR): 
-    path_wind = TI_1_DIR/(str(file))
+for file_name in files: 
+    # looping through the file names to set the path of the wind data for each data file 
+    path_wind = TI_1_DIR/(str(file_name))
+
+    # extracting associated wind speed from the file name to correlate the mean and stdv to the associated wind speed 
+    associated_spd = int(file_name.split('_')[1])
 
     #   use load_wind to load the wind data 
     t_wind, u_wind = codecamp.load_wind(path_wind)
@@ -61,14 +72,13 @@ for file in os.listdir(TI_1_DIR):
     std_tower = np.std(xt)
 
     #  append tower_mean_stdv and blade_mean_stdv with mean and stdv values
-    blade_data.append([mean_blade,std_blade])
-    tower_data.append([mean_tower, std_tower])
+    blade_data_TI_01.append([associated_spd,mean_blade,std_blade])
+    tower_data_TI_01.append([associated_spd,mean_tower, std_tower])
 # end loop 
 
-blade_mean_stdv = np.array(blade_data)
-tower_mean_stdv = np.array(tower_data)
-print(blade_mean_stdv)
-print(tower_mean_stdv)
+blade_mean_stdv_TI_01 = np.array(blade_data_TI_01)
+tower_mean_stdv_TI_01 = np.array(tower_data_TI_01)
+
 
 # step 3:
 # create two figures (see reference link for method for plotting): 
